635781664d031d08e58b91f4faad50b95c86e25c
Omar-Belghaouti <bel_omar18@yahoo.com>
2019-03-20 12:29:58 +0100

    Face detection using Emgu CV

--- 25_Face_Detection/Form1.Designer.cs
+++ 25_Face_Detection/Form1.Designer.cs
@@ -28,13 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.detectFaceHaarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.detectFaceLBPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.detectFaceHaarToolStripMenuItem,
+            this.detectFaceLBPToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // detectFaceHaarToolStripMenuItem
+            // 
+            this.detectFaceHaarToolStripMenuItem.Name = "detectFaceHaarToolStripMenuItem";
+            this.detectFaceHaarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.detectFaceHaarToolStripMenuItem.Text = "Detect Face Haar";
+            this.detectFaceHaarToolStripMenuItem.Click += new System.EventHandler(this.detectFaceHaarToolStripMenuItem_Click);
+            // 
+            // detectFaceLBPToolStripMenuItem
+            // 
+            this.detectFaceLBPToolStripMenuItem.Name = "detectFaceLBPToolStripMenuItem";
+            this.detectFaceLBPToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.detectFaceLBPToolStripMenuItem.Text = "Detect Face LBP";
+            this.detectFaceLBPToolStripMenuItem.Click += new System.EventHandler(this.detectFaceLBPToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 426);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Face Detection";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem detectFaceHaarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem detectFaceLBPToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- 25_Face_Detection/Form1.cs
+++ 25_Face_Detection/Form1.cs
@@ -3,18 +3,141 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Emgu.CV;
+using Emgu.CV.Structure;
+
 namespace _25_Face_Detection
 {
     public partial class Form1 : Form
     {
+        Image<Bgr, byte> imgInput;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    imgInput = new Image<Bgr, byte>(ofd.FileName);
+                    pictureBox1.Image = imgInput.Bitmap;
+                }
+                else
+                {
+                    throw new Exception("No file selected.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void detectFaceHaarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (imgInput == null)
+                {
+                    throw new Exception("Please select an Image.");
+                }
+                DetectFaceHaar();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void DetectFaceHaar()
+        {
+            try
+            {
+                string facePath = Path.GetFullPath(@"../../Harcascade classifiers/haarcascade_frontalface_default.xml");
+                string eyePath = Path.GetFullPath(@"../../Harcascade classifiers/haarcascade_eye.xml");
+
+                CascadeClassifier classifierFace = new CascadeClassifier(facePath); // Aspects a grayscale Image
+                CascadeClassifier classifierEye = new CascadeClassifier(eyePath);
+
+                var imgGray = imgInput.Convert<Gray, byte>().Clone();
+
+                Rectangle[] faces = classifierFace.DetectMultiScale(imgGray, 1.1, 4);
+
+                foreach (var face in faces)
+                {
+                    imgInput.Draw(face, new Bgr(0, 0, 255), 2);
+                    // Find eyes inside faces, limit the ROI
+                    imgGray.ROI = face;
+                    Rectangle[] eyes = classifierEye.DetectMultiScale(imgGray, 1.1, 4);
+                    foreach (var eye in eyes)
+                    {
+                        // Retrieve the exact location for the full image
+                        var e = eye;
+                        e.X += face.X;
+                        e.Y += face.Y;
+                        imgInput.Draw(e, new Bgr(255, 0, 0), 2);
+                    }
+                }
+                pictureBox1.Image = imgInput.Bitmap;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void detectFaceLBPToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (imgInput == null)
+                {
+                    throw new Exception("Please select an image.");
+                }
+                DetectFaceLBP();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// local binary patterns method
+        /// </summary>
+        private void DetectFaceLBP()
+        {
+            try
+            {
+                string facePath = Path.GetFullPath(@"../../LBPcascade classifiers/lbpcascade_frontalface.xml");
+
+                // the following class is smart enough to know the type of classifier based from XML files
+                CascadeClassifier classifierFace = new CascadeClassifier(facePath);
+
+                var imgGray = imgInput.Convert<Gray, byte>().Clone();
+                Rectangle[] faces = classifierFace.DetectMultiScale(imgGray, 1.1, 2);
+                foreach (var face in faces)
+                {
+                    imgInput.Draw(face, new Bgr(255, 0, 0));
+                }
+
+                pictureBox1.Image = imgInput.Bitmap;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
         }
     }
 }
